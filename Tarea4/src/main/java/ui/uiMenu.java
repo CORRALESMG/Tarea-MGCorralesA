d519bc594e3875c6ea41a3586233980c99f403ed
CORRALESMG <110306369+CORRALESMG@users.noreply.github.com>
2022-10-15 01:38:44 -0600

    Add files via upload

---
+++ Tarea4/src/main/java/ui/uiMenu.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ui;
+
+/**
+ *
+ * @author JADPA-10
+ */
+public interface uiMenu {
+    void mostrar();
+    void Selecionar();
+    void regresar();
+}
